d03544a00ebe49d0762da1a53f21ebd763bfd8e8
Bruno Patrício <bpatriciocosta@gmail.com>
2023-02-02 15:35:11 -0300

    Theme colors config

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+const theme = {
+  primary: '#DF71AA',
+  secondary: '#88F4FF',
+  terciary: '#7338AC',
+  primaryFontColor: '#88F4FF',
+  secondaryFontColor: '#DF71AA',
+  backgroundColor: '#7338AC'
+}
+
+export default theme
