e166db3b5c32c75948a74a722346beea9345c7a4
Bruno Patrício <bpatriciocosta@gmail.com>
2023-02-02 15:23:09 -0300

    _app config

---
+++ pages/_app.jsx
@@ -0,0 +1,24 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import theme from '../src/theme'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  body {
+    font-family: 'Unbounded', cursive;
+  }
+`
+
+function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
+
+export default App
