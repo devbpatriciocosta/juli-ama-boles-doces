909bbf8b5d85517408a55571d720722da7ddbe6f
Bruno Patrício <bpatriciocosta@gmail.com>
2023-02-03 17:03:00 -0300

    Icon Images

---
+++ src/components/iconImages/IconImages.jsx
@@ -0,0 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+export default function IconImages({ imageName, type }) {
+  return <img src={`images/${imageName}.${type}`} />
+}
