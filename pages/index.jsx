09afcfed1cb12248f3fc6cc797778a37ced9f6b8
Bruno Patrício <bpatriciocosta@gmail.com>
2023-02-04 05:36:03 -0300

    header and nav bar on index

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,21 @@
+import styled from 'styled-components'
+
+import IconImage from '../src/components/iconImages/IconImages'
+import Navbar from '../src/components/navbar/Navbar'
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 30px 0 0 80px;
+`
+
 export default function confeitariaApp() {
-  return <h1>Hello, Juli Ama - Doces & Bolos</h1>
+  return (
+    <>
+      <Header>
+        <IconImage imageName="logoJuliama" type="svg" />
+        <Navbar />
+      </Header>
+    </>
+  )
 }
