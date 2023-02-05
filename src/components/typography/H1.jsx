a1708d78cfe5e1e6c3d2ffa3e6b8074b82b3823e
Bruno Patrício <bpatriciocosta@gmail.com>
2023-02-05 16:38:57 -0300

    H1 e H3 - Typograph

---
+++ src/components/typography/H1.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const H1 = styled.h1`
+  font-size: 60px;
+  font-family: 'Gabriela';
+  font-style: normal;
+  font-weight: 600;
+  color: ${(props) => props.theme.secondary};
+`
+export default H1
---
+++ src/components/typography/H3.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const H3 = styled.h1`
+  font-size: 32px;
+  font-family: 'Gabriela';
+  font-style: normal;
+  font-weight: 300;
+  color: ${(props) => props.theme.secondary};
+`
+export default H3
