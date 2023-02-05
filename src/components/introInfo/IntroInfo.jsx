a474d4defc9f1dabad05dff8087f3d67562f883c
Bruno Patrício <bpatriciocosta@gmail.com>
2023-02-05 16:38:45 -0300

    Adding a secondary intro info

--- src/components/introInfo/IntroInfo.jsx
+++ src/components/introInfo/IntroInfo.jsx
@@ -1,16 +1,38 @@
 import styled from 'styled-components'
 
-const TitleIntroInfo = styled.div`
+import H1 from '../typography/H1'
+import H3 from '../typography/H3'
+import SecondNavbar from '../navbar/SecondNavbar'
+
+const IntroInfoMainContainer = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
-  font-size: 32px;
+  border-bottom: 2px dashed ${(props) => props.theme.secondary};
+`
+
+const TitleIntroInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+const SecondNavbarContainer = styled.div`
+  margin-top: 100px;
 `
 
 export default function IntroInfo() {
   return (
-    <TitleIntroInfo>
-      <h1>Seu dia muito mais doce!</h1>
-    </TitleIntroInfo>
+    <IntroInfoMainContainer>
+      <TitleIntroInfo>
+        <H1>Seu dia muito mais doce!</H1>
+        <H3>Produtos produzidos artesanalmente com amor e carinho</H3>
+      </TitleIntroInfo>
+      <SecondNavbarContainer>
+        <SecondNavbar />
+      </SecondNavbarContainer>
+    </IntroInfoMainContainer>
   )
 }
