38f63c3b371ceb8f9f34d6cba863036ab324b3f5
Bruno Patrício <bpatriciocosta@gmail.com>
2023-02-05 16:38:19 -0300

    Creating a Secondary Navbar to the products

---
+++ src/components/navbar/SecondNavbar.jsx
@@ -0,0 +1,72 @@
+import styled from 'styled-components'
+
+const MainContainer = styled.nav`
+  color: ${(props) => props.theme.secondary};
+  width: 100%;
+  min-height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+`
+
+const MainList = styled.ul`
+  display: flex;
+  gap: 100px;
+  list-style: none;
+
+  li {
+    cursor: pointer;
+    transition: all 0.3s;
+    position: relative;
+  }
+
+  li:after {
+    content: '';
+    position: absolute;
+    background-color: ${(props) => props.theme.secondary};
+    height: 2px;
+    width: 0;
+    left: 0;
+    bottom: -10px;
+    transition: 0.3s;
+  }
+
+  li:hover:after {
+    width: 100%;
+  }
+`
+
+const SecondaryList = styled.li`
+  font-size: 24px;
+  font-family: 'Gabriela';
+  font-style: normal;
+  font-weight: 600;
+  line-height: 10px;
+`
+
+const TagA = styled.a`
+  text-decoration: none;
+  cursor: pointer;
+`
+
+export default function Navbar() {
+  return (
+    <MainContainer>
+      <MainList>
+        <SecondaryList>
+          <TagA>Doces Tradicionais</TagA>
+        </SecondaryList>
+        <SecondaryList>
+          <TagA>Doces Finos</TagA>
+        </SecondaryList>
+        <SecondaryList>
+          <TagA>Bolos & Tortas</TagA>
+        </SecondaryList>
+        <SecondaryList>
+          <TagA>Biscoitos & Merengues</TagA>
+        </SecondaryList>
+      </MainList>
+    </MainContainer>
+  )
+}
