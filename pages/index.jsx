3f5d7f53e50a72f00579fd8efa98a5b63a59b481
Bruno Patrício <bpatriciocosta@gmail.com>
2023-02-06 10:56:26 -0300

    adding info container + styles

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,11 +3,22 @@ import styled from 'styled-components'
 import IconImage from '../src/components/iconImages/IconImages'
 import Navbar from '../src/components/navbar/Navbar'
 import IntroInfo from '../src/components/introInfo/IntroInfo'
+import TimeInfos from '../src/components/timeInfos/TimeInfos'
 
 const Header = styled.div`
   display: flex;
   align-items: center;
   margin: 30px 0 0 80px;
+`
+
+const InfoContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+`
+
+const IntroInfoContainer = styled.div`
+  margin-top: -335px;
+  width: 1500px;
 `
 
 export default function confeitariaApp() {
@@ -17,7 +28,12 @@ export default function confeitariaApp() {
         <IconImage imageName="logoJuliama" type="svg" />
         <Navbar />
       </Header>
-      <IntroInfo />
+      <InfoContainer>
+        <TimeInfos />
+        <IntroInfoContainer>
+          <IntroInfo />
+        </IntroInfoContainer>
+      </InfoContainer>
     </>
   )
 }
