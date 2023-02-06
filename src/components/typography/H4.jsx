f9cc81bb4e30121633d5c521c488f21c6fccc931
Bruno Patrício <bpatriciocosta@gmail.com>
2023-02-06 10:55:34 -0300

    H4 Creation on typograph

---
+++ src/components/typography/H4.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const H4 = styled.h1`
+  font-size: 26px;
+  font-family: 'Gabriela';
+  font-style: normal;
+  font-weight: 300;
+  color: ${(props) => props.theme.primary};
+`
+export default H4
