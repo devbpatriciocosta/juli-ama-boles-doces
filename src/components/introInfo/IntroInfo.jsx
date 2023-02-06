1fb50366dc373048cfb33f221c9073057731b1d7
Bruno Patrício <bpatriciocosta@gmail.com>
2023-02-06 10:56:12 -0300

    Intro info main container margin

--- src/components/introInfo/IntroInfo.jsx
+++ src/components/introInfo/IntroInfo.jsx
@@ -9,6 +9,7 @@ const IntroInfoMainContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  margin-top: 350px;
   border-bottom: 2px dashed ${(props) => props.theme.secondary};
 `
 
