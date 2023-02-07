4b0de417cadc97aea5d08064203b18f9b4c15bd5
Bruno Patrício <bpatriciocosta@gmail.com>
2023-02-07 12:17:29 -0300

    H5 adding

---
+++ src/components/typography/H5.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const H5 = styled.h1`
+  font-size: 16px;
+  font-family: 'Gabriela';
+  font-style: normal;
+  font-weight: 300;
+  color: ${(props) => props.theme.secondary};
+`
+export default H5
