32169109c1d9a2d42b156097006b8b27d0e53761
Bruno Patrício <bpatriciocosta@gmail.com>
2023-02-07 11:21:55 -0300

    Products session

---
+++ src/components/products/Products.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+import H4 from '../typography/H4'
+
+export default function Products() {
+  return (
+    <>
+      <H4>Mais vendidos</H4>
+    </>
+  )
+}
