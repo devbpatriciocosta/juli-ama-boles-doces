47e282fb39caa6545c5b3b7b31d3c0d5dd75cc6d
Bruno Patrício <bpatriciocosta@gmail.com>
2023-02-10 11:46:55 -0300

    Colors adjustments

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -12,9 +12,9 @@ const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
   margin: 20px 0;
-  gap: 10px;
+  gap: 20px;
   align-items: center;
-  width: 350px;
+  width: 450px;
 
   @media (max-width: 425px) {
     width: 300px;
@@ -26,10 +26,10 @@ const Button = styled.button`
   cursor: pointer;
   width: 188px;
   height: 35px;
-  border: 2px solid white;
+  border: 2px solid ${(props) => props.theme.secondary};
   border-radius: 20px;
-  background-color: hsla(630, 60%, 50%, 0.9);
-  color: white;
+  background-color: ${(props) => props.theme.terciary};
+  color: ${(props) => props.theme.secondary};
   display: flex;
   align-items: center;
   justify-content: center;
@@ -37,17 +37,9 @@ const Button = styled.button`
   font-size: 20px;
   font-weight: bold;
   margin-top: 20px;
-  ${(props) => !props.disabled && 'cursor: pointer'}
+
   :hover {
-    box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24), 0 17px 50px 0 rgba(0, 0, 0, 0.19);
-    background-color: #3e8e41;
-  }
-  :active {
-    box-shadow: 0 5px #666;
-    transform: translateY(2px);
-  }
-  :disabled {
-    background-color: #666;
+    background-color: ${(props) => props.theme.primary};
   }
 `
 
@@ -65,7 +57,7 @@ export default function Form() {
   const handleForm = async (data) => {
     try {
       const { status } = await axios.post(
-        `${process.env.NEXT_PUBLIC_API_URL}/api/user/signIn`,
+        `${process.env.NEXT_PUBLIC_API_URL}/api/user/signUp`,
         data
       )
       if (status === 201) {
