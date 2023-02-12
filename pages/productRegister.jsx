56b8a344f2426a6b1a996c1423edb6861add104b
Bruno Patrício <bpatriciocosta@gmail.com>
2023-02-12 19:57:27 -0300

    Creating product register session

---
+++ pages/productRegister.jsx
@@ -0,0 +1,56 @@
+import styled from 'styled-components'
+
+import IconImage from '../src/components/iconImages/IconImages'
+import H3 from '../src/components/typography/H3'
+
+import LoginForm from '../src/components/form/LoginForm'
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 30px 0 0 80px;
+`
+
+const MainContainer = styled.nav`
+  background-color: ${(props) => props.theme.primary};
+  color: ${(props) => props.theme.secondary};
+  width: 100%;
+  min-height: 40px;
+  margin-left: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+
+  clip-path: polygon(100% 0, 100% 50%, 100% 100%, 0% 100%, 2% 50%, 0% 0%);
+`
+
+const SecondaryContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 20px;
+  background-color: none;
+  width: 600px;
+  min-height: 500px;
+  margin-left: 850px;
+  border: 2px dashed ${(props) => props.theme.secondary};
+`
+
+export default function SignUp() {
+  return (
+    <>
+      <Header>
+        <IconImage imageName="logoJuliama" type="svg" />
+        <MainContainer>
+          <H3>Olá, faça Login</H3>
+        </MainContainer>
+      </Header>
+      <SecondaryContainer>
+        <H3>Login de administrador</H3>
+        <LoginForm />
+      </SecondaryContainer>
+    </>
+  )
+}
