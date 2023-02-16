faf330b7e0983e6fe8cd617d7fc958489b06dd6e
Bruno Patrício <bpatriciocosta@gmail.com>
2023-02-16 17:26:25 -0300

    H2 on Typograph

---
+++ src/components/typography/H2.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const H2 = styled.h2`
+  font-size: 46px;
+  font-family: 'Gabriela';
+  font-style: normal;
+  font-weight: 600;
+  color: ${(props) => props.theme.secondary};
+`
+export default H2
